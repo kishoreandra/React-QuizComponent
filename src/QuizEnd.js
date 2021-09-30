c47c27d4fb8bbeafb35666159e74ad9569f7476a
kishore <keechu1008@gmail.com>
2021-09-30 09:30:00 +0000

    module 4

---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class QuiEnd extends Component{
+    render(){
+        return(
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+}
+
+export default QuiEnd;
