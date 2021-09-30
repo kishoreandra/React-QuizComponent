ebf431a1fadd89d24ebe470d7ed29b18ac9d2c70
kishore <keechu1008@gmail.com>
2021-09-30 10:27:26 +0000

    removed arrow funcs

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component{
-    handleClick = (buttonText) => {
+    handleClick (buttonText) {
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler()
         }
@@ -14,7 +14,7 @@ class QuizQuestion extends Component{
         <section>
         <p>{this.props.quiz_question.instruction_text}</p>
         </section>
-        <section className="buttons">
+        <section className="but tons">
           <ul>
             {
                 this.props.quiz_question.answer_options.map((answer_option ,index)=> {
