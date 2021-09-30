a53c0a35149ca16d140398850a5674dfef4e3351
kishore <keechu1008@gmail.com>
2021-09-30 10:22:12 +0000

    mod -6 changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,11 @@ class Quiz extends Component{
         super(props)
         this.state = {quiz_position: 1}
     }
-    showNextQuestion = ()=>{
-        this.setState({quiz_position : this.state.quiz_position + 1})
-    }
+    showNextQuestion() {
+        this.setState((state) => {
+          return { quiz_position: state.quiz_position + 1 }
+        })
+      }
     render(){
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
         return(
